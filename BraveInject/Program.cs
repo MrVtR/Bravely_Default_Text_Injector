d1798ff1f4f9d8ef3baf95ac13b120cd4ecea82d
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-07-28 00:10:27 -0300

    Código automatizado para arquivos de texto
    Código feito para automatizar a tarefa de inserir textos,agora,é possivel inserir um arquivo .txt de uma só vez,ao invés de inserir texto por texto na linha de comando.

--- BraveInject/Program.cs
+++ BraveInject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Komponent.IO;
 namespace ConsoleApp1
@@ -12,7 +13,6 @@ namespace ConsoleApp1
 
             try
             {
-                string aux = "";
                 int i = 1;
                 if (args.Length < 1)
                 {
@@ -20,90 +20,78 @@ namespace ConsoleApp1
                     return;
                 }
 
-                var isExtract = args.Length > 1 && args[1] == "-x";
-                if (args.Length > 1 && args[1] != "-x")
+                var isExtract = args.Length > 1 && args[0] == "-x";
+                var filtro = args.Length > 1 && args[0] == "-f";
+                if ((args.Length > 1 && args[0] != "-x") && (args.Length > 1 && args[0] != "-f"))
                 {
                     Console.WriteLine($"Opção desconhecida {args[1]}.");
                     return;
                 }
-                while (i == 1)
+                if (isExtract)
                 {
-                    BinaryWriterX BravelyFile;
-                    //Verifica a existencia do arquivo no sistema
-                    if (System.IO.File.Exists(@"C:\Users\vitor\Desktop\" + args[0]))
+                    while (i == 1)
                     {
-                        string fileName = @"C:\Users\vitor\Desktop\" + args[0];
-                        FileStream writeStream = new FileStream(fileName, FileMode.Append);// Append -> Permite o Acréscimo de dados no arquivo
-                        BravelyFile = new BinaryWriterX(writeStream, Encoding.Unicode);
-                    }
-                    else
-                    {
-                        string fileName = @"C:\Users\vitor\Desktop\" + args[0];
-                        FileStream writeStream = new FileStream(fileName, FileMode.Create);//Criação do arquivo
-                        BravelyFile = new BinaryWriterX(writeStream, Encoding.Unicode);
-                    }
+                        BinaryWriterX BravelyFile;
+                        //Verifica a existencia do arquivo no sistema
+                        if (System.IO.File.Exists(@"C:\Users\vitor\Desktop\" + args[1]))
+                        {
+                            string fileName = @"C:\Users\vitor\Desktop\" + args[1];
+                            FileStream writeStream = new FileStream(fileName, FileMode.Append);// Append -> Permite o Acréscimo de dados no arquivo
+                            BravelyFile = new BinaryWriterX(writeStream, Encoding.Unicode);
+                        }
+                        else
+                        {
+                            string fileName = @"C:\Users\vitor\Desktop\" + args[1];
+                            FileStream writeStream = new FileStream(fileName, FileMode.Create);//Criação do arquivo
+                            BravelyFile = new BinaryWriterX(writeStream, Encoding.Unicode);
+                        }
 
-                    //Verifica se o texto possui \n ou não
-                    Console.WriteLine("O texto a ser inserido possui \\n?");
-                    aux = Console.ReadLine();
-                    if (aux == "s")
-                        BravelyWriteComLinhaN(args, BravelyFile);
-                    else
-                        BravelyWriteSemLinhaN(args, BravelyFile);
+                        string[] lines = System.IO.File.ReadAllLines(@"C:\Users\vitor\Desktop\" + args[2]);
 
-                    Console.WriteLine("Existem mais textos para colocar?");
-                    aux = Console.ReadLine();
-                    if (aux == "n")
+                        for (int j = 0; j < lines.Length; j++)
+                        {
+                            if (lines[j] != "{END}")//Verifica se chegou ao fim de uma sub-string do arquivo
+                            {
+                                if (lines[j].Contains("/0A"))
+                                {
+                                    lines[j] = lines[j].Replace("/0A", "\n");
+                                }
+
+                                BravelyFile.WriteString(lines[j] + '\0', Encoding.Unicode, false, false);//Comando para colocar o texto no arquivo e com byte 00 a cada letra,com \n ao fim da string
+                            }
+                            else
+                                continue;
+                        }
+                        Console.WriteLine("Foi");
+                        BravelyFile.WriteString("" + '\0', Encoding.Unicode, false, false);//Finaliza o arquivo colocando 00 00 nele --> Arrumar
+                        BravelyFile.Close();
                         i = 0;
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-        }
-        public static void BravelyWriteComLinhaN(string[] args, BinaryWriterX BravelyFile)
-        {
-            try
-            {
-                string texto = "";
-
-                //Laço para colocar os textos do jogo(Por enquanto,cada entrada deve ser colocada manualmente)
-
-                while (!string.IsNullOrEmpty(texto = Console.ReadLine()))
+                if(filtro)
                 {
-                    BravelyFile.WriteString(texto + '\n', Encoding.Unicode, false, false);//Comando para colocar o texto no arquivo e com byte 00 a cada letra,com \n ao fim da string
-                }
-                BravelyFile.WriteString("" + '\0', Encoding.Unicode, false, false);//Finaliza o arquivo colocando 00 00 nele --> Arrumar
-                BravelyFile.Close();
-            }
+                    Console.WriteLine("foi");
+                    string[] lines = System.IO.File.ReadAllLines(@"C:\Users\vitor\Desktop\" + args[1]);
 
+                    int contEnd = 1;
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+                        
+                        if(lines[j].StartsWith("{END}"))
+                        {
+                            contEnd++;
+                        }
+                            
+                        if (lines[j].StartsWith("/00/00"))//Filtrando ponteiros corrompidos(padrão identificado)
+                            Console.WriteLine("Posição do ponteiro corrompido: " + contEnd);
+                    }
+                }
+            }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-
-        }
-        public static void BravelyWriteSemLinhaN(string[] args, BinaryWriterX BravelyFile)
-        {
-            try
-            {
-                string texto = "";
-
-                while (!string.IsNullOrEmpty(texto = Console.ReadLine()))
-                {
-                    BravelyFile.WriteString(texto + '\0', Encoding.Unicode, false, false);//Comando para colocar o texto no arquivo e com byte 00 a cada letra,com \n ao fim da string
-                }
-                BravelyFile.WriteString("" + '\0', Encoding.Unicode, false, false);//Finaliza o arquivo colocando 00 00 nele --> Arrumar
-                BravelyFile.Close();
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
         }
     }
-}
+    }
+       
