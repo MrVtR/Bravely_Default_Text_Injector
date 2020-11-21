7f3d078fec31d76cf61bf160998672905e08940b
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-11-21 15:54:34 -0300

    Finalizado versão gráfica do aplicativo

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Bravely_Default_Text_Injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnTexto = new System.Windows.Forms.Button();
+            this.btnPonteiros = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.repositórioDoCódigoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contatoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTexto
+            // 
+            this.btnTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTexto.Location = new System.Drawing.Point(162, 341);
+            this.btnTexto.Name = "btnTexto";
+            this.btnTexto.Size = new System.Drawing.Size(123, 37);
+            this.btnTexto.TabIndex = 0;
+            this.btnTexto.Text = "Injetar Texto";
+            this.btnTexto.UseVisualStyleBackColor = true;
+            this.btnTexto.Click += new System.EventHandler(this.btnTexto_Click);
+            // 
+            // btnPonteiros
+            // 
+            this.btnPonteiros.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPonteiros.Location = new System.Drawing.Point(444, 341);
+            this.btnPonteiros.Name = "btnPonteiros";
+            this.btnPonteiros.Size = new System.Drawing.Size(151, 37);
+            this.btnPonteiros.TabIndex = 2;
+            this.btnPonteiros.Text = "Filtrar Ponteiros";
+            this.btnPonteiros.UseVisualStyleBackColor = true;
+            this.btnPonteiros.Click += new System.EventHandler(this.btnPonteiros_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajudaToolStripMenuItem,
+            this.repositórioDoCódigoToolStripMenuItem,
+            this.contatoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(737, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem,
+            this.comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem
+            // 
+            this.comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem.Name = "comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem";
+            this.comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem.Size = new System.Drawing.Size(315, 24);
+            this.comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem.Text = "Como utilizar a edição do Crowd.fs";
+            this.comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem.Click += new System.EventHandler(this.comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem_Click);
+            // 
+            // comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem
+            // 
+            this.comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem.Name = "comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem";
+            this.comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem.Size = new System.Drawing.Size(315, 24);
+            this.comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem.Text = "Como utilizar a edição do Index.fs";
+            this.comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem.Click += new System.EventHandler(this.comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem_Click);
+            // 
+            // repositórioDoCódigoToolStripMenuItem
+            // 
+            this.repositórioDoCódigoToolStripMenuItem.Name = "repositórioDoCódigoToolStripMenuItem";
+            this.repositórioDoCódigoToolStripMenuItem.Size = new System.Drawing.Size(172, 24);
+            this.repositórioDoCódigoToolStripMenuItem.Text = "Repositório do código";
+            this.repositórioDoCódigoToolStripMenuItem.Click += new System.EventHandler(this.repositórioDoCódigoToolStripMenuItem_Click);
+            // 
+            // contatoToolStripMenuItem
+            // 
+            this.contatoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contatoToolStripMenuItem.Name = "contatoToolStripMenuItem";
+            this.contatoToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.contatoToolStripMenuItem.Text = "Contato";
+            this.contatoToolStripMenuItem.Click += new System.EventHandler(this.contatoToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(737, 914);
+            this.Controls.Add(this.btnPonteiros);
+            this.Controls.Add(this.btnTexto);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bravely Default - Crowd.fs and Index.fs Injector";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTexto;
+        private System.Windows.Forms.Button btnPonteiros;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem contatoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem repositórioDoCódigoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using Komponent.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bravely_Default_Text_Injector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void contatoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://github.com/MrVtR");
+            Process.Start(sInfo);
+        }
+
+        private void repositórioDoCódigoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://github.com/MrVtR/Bravely_Default_Text_Injector");
+            Process.Start(sInfo);
+        }
+
+        private void comoUtilizarAEdiçãoDoCrowdfsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tutorial:", "Tutorial de uso da edição do Crowd.fs", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void comoUtilizarAEdiçãoDoIndexfsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tutorial:", "Tutorial de uso da edição do Index.fs", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnTexto_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Selecione o arquivo de texto com o formato de output do Kruptar7";
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Arquivos de Texto|*.txt";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.RestoreDirectory = true;
+            DialogResult drOpen = openFileDialog1.ShowDialog();
+
+            saveFileDialog1.Title = "Selecione o nome do arquivo que será criado com o inject do Texto";
+            saveFileDialog1.RestoreDirectory = true;
+            DialogResult drSave = saveFileDialog1.ShowDialog();
+
+            if (drOpen == DialogResult.OK && drSave == DialogResult.OK)
+            {
+                int i = 1;
+                while (i == 1)
+                {
+                    BinaryWriterX BravelyFile;
+                    //Verifica a existencia do arquivo no sistema
+                    if (System.IO.File.Exists(saveFileDialog1.FileName))
+                    {
+                        string fileName = saveFileDialog1.FileName;
+                        FileStream writeStream = new FileStream(fileName, FileMode.Append);// Append -> Permite o Acréscimo de dados no arquivo
+                        BravelyFile = new BinaryWriterX(writeStream, Encoding.Unicode);
+                    }
+                    else
+                    {
+                        string fileName = saveFileDialog1.FileName;
+                        FileStream writeStream = new FileStream(fileName, FileMode.Create);//Criação do arquivo
+                        BravelyFile = new BinaryWriterX(writeStream, Encoding.Unicode);
+                    }
+
+                    string[] lines = System.IO.File.ReadAllLines(openFileDialog1.FileName);
+
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+                        if (lines[j] != "{END}")//Verifica se chegou ao fim de uma sub-string do arquivo
+                        {
+                            int cont = j;
+                            if (cont + 1 > lines.Length)
+                            {
+                                if (lines[j].Contains("/0A"))
+                                {
+                                    lines[j] = lines[j].Replace("/0A", "\n");
+                                }
+                                BravelyFile.WriteString(lines[j] + '\0', Encoding.Unicode, false, false);//Comando para colocar o texto no arquivo e com byte 00 a cada letra,com \n ao fim da string
+                                Console.WriteLine("passou");
+                                return;
+                            }
+                            else if (lines[cont] != "" && cont + 1 <= lines.Length)//Verifica se a próxima linha tem textos,se tiver,quer dizer que é necessário quebra de linha
+                            {
+                                if (lines[j].Contains("/0A"))
+                                {
+                                    lines[j] = lines[j].Replace("/0A", "\n");
+                                }
+                                BravelyFile.WriteString(lines[j] + '\n', Encoding.Unicode, false, false);//Comando para colocar o texto no arquivo e com byte 00 a cada letra,com \n ao fim da string
+                            }
+                            else
+                            {
+                                if (lines[j].Contains("/0A"))
+                                {
+                                    lines[j] = lines[j].Replace("/0A", "\n");
+                                }
+                                BravelyFile.WriteString(lines[j] + '\0', Encoding.Unicode, false, false);//Comando para colocar o texto no arquivo e com byte 00 a cada letra,com \n ao fim da string
+                            }
+                        }
+                        else
+                            continue;
+                    }
+                    Console.WriteLine("Foi");
+                    BravelyFile.WriteString("" + '\0', Encoding.Unicode, false, false);//Finaliza o arquivo colocando 00 00 nele --> Arrumar
+                    BravelyFile.Close();
+                    i = 0;
+                }
+            }
+            MessageBox.Show(saveFileDialog1.FileName + "\nCriado com sucesso\nCom o Inject de: "+openFileDialog1.FileName, "Operação finalizada");
+            var continuar = MessageBox.Show("Quer fazer outra operação?", "Continuar?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (continuar == DialogResult.No)
+                Form1.ActiveForm.Close();
+
+        }
+
+        private void btnPonteiros_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Selecione o arquivo de texto com o formato de output do Kruptar7";
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Arquivos de Texto|*.txt";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.RestoreDirectory = true;
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    Console.WriteLine("foi");
+                    string[] lines = System.IO.File.ReadAllLines(openFileDialog1.FileName);
+
+                    int contEnd = 1;
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+
+                        if (lines[j].StartsWith("{END}"))
+                        {
+                            contEnd++;
+                        }
+
+                        if (lines[j].StartsWith("/00/00"))//Filtrando ponteiros corrompidos(padrão identificado)
+                        {
+                            Console.WriteLine("Posição do ponteiro corrompido: " + (contEnd));
+                            contEnd--;
+                        }
+
+                    }
+                    MessageBox.Show(openFileDialog1.FileName+"\nFiltrado com sucesso", "Operação finalizada");
+                    var continuar = MessageBox.Show("Quer fazer outra operação?", "Continuar?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (continuar == DialogResult.No)
+                        Form1.ActiveForm.Close();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+        }
+    }
+}
